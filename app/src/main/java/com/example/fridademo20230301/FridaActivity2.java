b37e7ddc6c2d7ff09a3f9a5b97b993492717824d
chenxiangan <1598828268@qq.com>
2023-11-29 18:02:58 +0800

    update

--- app/src/main/java/com/example/fridademo20230301/FridaActivity2.java
+++ app/src/main/java/com/example/fridademo20230301/FridaActivity2.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 import dalvik.system.DexClassLoader;
@@ -87,9 +88,56 @@ public class FridaActivity2 extends AppCompatActivity implements View.OnClickLis
         return this.DynamicDexCheck;
     }
 
+
+    private Object loaddex2() {
+        try (InputStream is = getAssets().open("1.jpg")) {
+            byte[] jpgBytes = new byte[is.available()];
+            is.read(jpgBytes);
+            // 分离JPG文件内容和DEX文件内容
+            int jpgSize = indexOf(jpgBytes, new byte[]{(byte) 0xFF, (byte) 0xD9}) + 2;
+            byte[] jpgContent = Arrays.copyOfRange(jpgBytes, 0, jpgSize);
+            byte[] dexContent = Arrays.copyOfRange(jpgBytes, jpgSize, jpgBytes.length);
+
+            File dexFile = new File(getFilesDir(), "test2.dex");
+            try (OutputStream os1 = new FileOutputStream(new File(getFilesDir(), "2.jpg"));
+                 OutputStream os2 = new FileOutputStream(dexFile)) {
+                os1.write(jpgContent);
+                os2.write(dexContent);
+            }
+
+            String dexPath = dexFile.getAbsolutePath();
+            String dexOutputDir = getFilesDir().getAbsolutePath();
+
+            ClassLoader classLoader = new DexClassLoader(dexPath, dexOutputDir, null, getClassLoader());
+            this.clazz = classLoader.loadClass("com.example.fridademo20230301.Dynamic.DynamicCheck");
+            this.DynamicDexCheck = this.clazz.newInstance();
+            this.DynamicDexCheck = clazz.newInstance();
+        } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        return this.DynamicDexCheck;
+    }
+    private static int indexOf(byte[] source, byte[] target) {
+        for (int i = 0; i < source.length - target.length + 1; i++) {
+            boolean match = true;
+            for (int j = 0; j < target.length; j++) {
+                if (source[i + j] != target[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public Object getDynamicDexCheck() {
         if (this.DynamicDexCheck == null) {
-            loaddex();
+            //如何loaddex2出问题就换loaddex()
+            loaddex2();
         }
         return this.DynamicDexCheck;
     }
